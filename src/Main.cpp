bdd304cb88523e057726f23adbb38ee3413f7530
Shann Delfin C. Salamingan IIII <134818470+Shann-Salamingan1048@users.noreply.github.com>
2024-04-26 14:48:18 +0800

    first try of opengl, silom nasab haha

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,42 @@
-#include <iostream>
-int main()
+#include <GLFW/glfw3.h>
+
+int main(void)
 {
-	std::cout << "Version: " << 1;
+
+
+    /* Initialize the library */
+    glfwInit();
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "First Time OpenGL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
