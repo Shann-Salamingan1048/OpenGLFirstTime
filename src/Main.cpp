b94830bcc3565579fef1ae05efc110bb72406a36
Shann Delfin C. Salamingan IIII <salaminganshann@gmail.com>
2024-05-01 16:03:26 +0800

    add

--- src/Main.cpp
+++ src/Main.cpp
@@ -24,17 +24,35 @@ int main(void)
         std::cout << "Error!\n";
     }
     std::cout << glGetString(GL_VERSION) << "\n";
+    
+    float positions[6] =
+    {
+        -0.5f, -0.5f, 
+         0.0f,  0.5f, 
+         0.5f, -0.5f, 
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); // create buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind bufffer, buffer of memory
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions,GL_STATIC_DRAW);
+
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+       /* glDrawElements(GL_TRIANGLES,3,)*/
+       /* glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();*/
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
